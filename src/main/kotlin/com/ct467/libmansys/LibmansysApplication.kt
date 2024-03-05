e3d173acc6ef9bf7d5dfaee5f5a798b9c6cbd256
bp82125 <103567877+bp82125@users.noreply.github.com>
2024-03-05 22:09:06 +0700

    inital commits

---
+++ src/main/kotlin/com/ct467/libmansys/LibmansysApplication.kt
@@ -0,0 +1,11 @@
+package com.ct467.libmansys
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibmansysApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibmansysApplication>(*args)
+}
---
+++ src/test/kotlin/com/ct467/libmansys/LibmansysApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ct467.libmansys
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibmansysApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
